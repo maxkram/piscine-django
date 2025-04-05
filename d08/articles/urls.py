681cf8743669b891bea57092bbc59079f1fb2a63
maxkram <max.kramer@live.com>
2025-04-05 10:55:14 +0500

    add ex01

--- d08/articles/urls.py
+++ d08/articles/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('articles/', views.ArticleListView.as_view(), name='articles'),
     path('login/', views.LoginView.as_view(), name='login'),
+    path('publications/', views.PublicationListView.as_view(), name='publications'),
+    path('article/<int:pk>/', views.ArticleDetailView.as_view(), name='detail'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('favourites/', views.FavouriteListView.as_view(), name='favourites'),
 ]
--- d08/articles/views.py
+++ d08/articles/views.py
@@ -1,6 +1,7 @@
-from django.views.generic import ListView, RedirectView
-from django.contrib.auth.views import LoginView as AuthLoginView
-from .models import Article
+from django.views.generic import ListView, DetailView, RedirectView
+from django.contrib.auth.views import LoginView as AuthLoginView, LogoutView as AuthLogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Article, UserFavouriteArticle
 
 class HomeView(RedirectView):
     url = '/articles/'
@@ -11,4 +12,23 @@ class ArticleListView(ListView):
 
 class LoginView(AuthLoginView):
     template_name = 'articles/login.html'
-    redirect_authenticated_user = True  
+    redirect_authenticated_user = True
+
+class PublicationListView(LoginRequiredMixin, ListView):
+    model = Article
+    template_name = 'articles/publication_list.html'
+    def get_queryset(self):
+        return Article.objects.filter(author=self.request.user)
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'articles/article_detail.html'
+
+class LogoutView(AuthLogoutView):
+    next_page = '/'
+
+class FavouriteListView(LoginRequiredMixin, ListView):
+    model = UserFavouriteArticle
+    template_name = 'articles/favourite_list.html'
+    def get_queryset(self):
+        return UserFavouriteArticle.objects.filter(user=self.request.user)
