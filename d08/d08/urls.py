69cc80c89ec63566f0e1dbf26fbdf6045e3f72aa
maxkram <max.kramer@live.com>
2025-04-07 15:13:12 +0500

    checked for compliance with the subject

--- d08/d08/urls.py
+++ d08/d08/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls.i18n import i18n_patterns
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include('articles.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
 )
